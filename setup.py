6ee2b53e2dd11ace1d1f503e2a41f0f7e29f0bf5
thematdev <thematdev@thematdev.org>
2023-02-28 00:26:06 +0300

    Fix problem index and report errors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='termforces',
-        version='0.1.0',
+        version='0.1.1',
         author='thematdev',
         author_email='thematdev@thematdev.org',
         description='CLI for Codeforces',
